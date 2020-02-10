698e3c4665f43593819864a61dd4903c2bd794fe
Kevin Cochran <kcochran@hashicorp.com>
2020-02-10 18:23:12 -0500

    remove module

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
     region = var.aws_region
 }
 
-# module "jenkinssg" {
-#   source  = "app.terraform.io/kevindemos/jenkinssg/aws"
-#   version = "1.0.2"
+module "jenkinssg" {
+  source  = "app.terraform.io/kevindemos/jenkinssg/aws"
+  version = "1.0.2"
 
-#   aws_access_key = var.aws_access_key
-#   aws_secret_key = var.aws_secret_key
-#   aws_region = var.aws_region
-# }
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  aws_region = var.aws_region
+}
 
